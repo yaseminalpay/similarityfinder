0e09de2ac4c65af5e982f4a967806eeaf1516bd8
Yasemin <ysmnalpay@gmail.com>
2018-09-15 18:26:13 +0300

    initial commit

---
+++ src/test/java/com/labfolder/similarityfinder/SimilarityfinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.labfolder.similarityfinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimilarityfinderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
